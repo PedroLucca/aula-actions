4e32305a2d52a00cfe1aff076525ee2ec892bf56
PedroLucca <pedrolucca27@gmail.com>
2021-06-06 15:15:10 -0300

    tarefa #1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Atividade Github Actions.
+          Atividade Github Actions - Novo workflow
         </p>
         <a
           className="App-link"
