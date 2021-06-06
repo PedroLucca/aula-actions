1be4cd0408273a53723c65aebafbd9f4402e5740
PedroLucca <pedrolucca27@gmail.com>
2021-06-06 15:23:01 -0300

    teste pull request

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Atividade Github Actions - Novo workflow atv 1
+          Atividade Github Actions - Testando pull request
         </p>
         <a
           className="App-link"
